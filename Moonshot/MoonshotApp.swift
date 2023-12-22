38ed9f5a01b3154543bba3c59db9b33212a4a0fb
Daniel Yankiver <dyankiver@gmail.com>
2023-12-22 14:50:23 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Daniel Yankiver on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

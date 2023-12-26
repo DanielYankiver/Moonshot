b91cd41e71166c12b88b98c7e161226c48d98345
Daniel Yankiver <dyankiver@gmail.com>
2023-12-26 12:03:10 -0500

    added astonauts data and images, and decoded

--- Moonshot/Bundle-Decodable.swift
+++ Moonshot/Bundle-Decodable.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+extension Bundle {
+  func decode(_ file: String) -> [String: Astronaut] {
+    guard let url = self.url(forResource: file, withExtension: nil) else {
+      fatalError("failed to locate \(file) in bundle.")
+    }
+
+    guard let data = try? Data(contentsOf: url) else {
+      fatalError("failed to load \(file) in bundle.")
+    }
+
+    let decoder = JSONDecoder()
+    
+    guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+      fatalError("failed to decode \(file) from bundle.")
+    }
+
+    return loaded
+  }
+}

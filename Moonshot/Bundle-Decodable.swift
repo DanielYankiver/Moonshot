37774269a2e6354dfdcdb86f6cb84d9f0e257248
Daniel Yankiver <dyankiver@gmail.com>
2023-12-26 14:58:20 -0500

    formatted data, styled cards and app

--- Moonshot/Bundle-Decodable.swift
+++ Moonshot/Bundle-Decodable.swift
@@ -8,19 +8,22 @@
 import Foundation
 
 extension Bundle {
-  func decode(_ file: String) -> [String: Astronaut] {
+  func decode<T: Codable>(_ file: String) -> T {
     guard let url = self.url(forResource: file, withExtension: nil) else {
-      fatalError("failed to locate \(file) in bundle.")
+      fatalError("Failed to locate \(file) in bundle.")
     }
 
     guard let data = try? Data(contentsOf: url) else {
-      fatalError("failed to load \(file) in bundle.")
+      fatalError("Failed to load \(file) from bundle.")
     }
 
     let decoder = JSONDecoder()
-    
-    guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
-      fatalError("failed to decode \(file) from bundle.")
+    let formatter = DateFormatter()
+    formatter.dateFormat = "y-MM-dd"
+    decoder.dateDecodingStrategy = .formatted(formatter)
+
+    guard let loaded = try? decoder.decode(T.self, from: data) else {
+      fatalError("Failed to decode \(file) from bundle.")
     }
 
     return loaded
